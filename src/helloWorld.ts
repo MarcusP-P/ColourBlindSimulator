84a87e791a12a0dd05038d469654d18204907656
Marcus Pallinger <git@marcus.pallinger.org>
2021-09-23 00:48:29 +1000

    Add ESLint

--- src/helloWorld.ts
+++ src/helloWorld.ts
@@ -1,8 +1,9 @@
 function helloWorld() {
-    const element= document.getElementById("hello-world");
-    
-    if (element) {
-      element.textContent = "Hello, World!";
-    }
+  const element = document.getElementById("hello-world");
+
+  if (element) {
+    element.textContent = "Hello, World!";
   }
-  helloWorld();
+}
+
+helloWorld();
