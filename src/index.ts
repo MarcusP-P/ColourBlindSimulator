ecd7ccaf3d377b169817e1197a1233c0b51f358f
Marcus Pallinger <git@marcus.pallinger.org>
2021-10-19 18:33:06 +1100

    Update to version 2.0.1 of ColorConverter
    This massively reduces loads now that we don't need

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import "./helloWorld.ts"
 
-import ColorConverter from 'cie-colorconverter'
+import { ColorConverter } from 'cie-colorconverter'
 
-let converter = new ColorConverter("D65");
+let converter = new ColorConverter()
+
+converter.RefWhite = "D65";
 
 let LuvNewNew = converter.RGB_to_Luv([119, 119, 119]);
 let RGBNewNew = converter.Luv_to_RGB([50, 0, 0]);
