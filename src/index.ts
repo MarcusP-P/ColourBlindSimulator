d9ef03627258417ffabf3909425c2ff1a1fabf8b
Marcus Pallinger <git@marcus.pallinger.org>
2021-10-19 21:36:31 +1100

    Fix unnused variables

--- src/index.ts
+++ src/index.ts
@@ -7,5 +7,8 @@ let converter = new ColorConverter()
 converter.RefWhite = "D65";
 
 let LuvNewNew = converter.RGB_to_Luv([119, 119, 119]);
+console.log(`RGB 119, 119, 119=Luv${LuvNewNew[0]}, ${LuvNewNew[1]}, ${LuvNewNew[2]}`);
 let RGBNewNew = converter.Luv_to_RGB([50, 0, 0]);
+console.log(`Luv 50, 0, 0=RGB${RGBNewNew[0]}, ${RGBNewNew[1]}, ${RGBNewNew[2]}`);
+
 helloWorld();
