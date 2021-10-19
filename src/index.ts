c05648b7d84b1d583cce140e24cf78881f8e99bd
Marcus Pallinger <git@marcus.pallinger.org>
2021-10-19 23:56:15 +1100

    Fix some style issues
    * add missing semicolons
    * use double quotes
    * use const for cariables that aren't reassigned

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
-import { helloWorld } from './helloWorld'
+import { helloWorld } from "./helloWorld";
 
-import { ColorConverter } from 'cie-colorconverter'
+import { ColorConverter } from "cie-colorconverter";
 
-let converter = new ColorConverter()
+const converter = new ColorConverter();
 
 converter.RefWhite = "D65";
 
-let LuvNewNew = converter.RGB_to_Luv([119, 119, 119]);
+const LuvNewNew = converter.RGB_to_Luv([119, 119, 119]);
 console.log(`RGB 119, 119, 119=Luv${LuvNewNew[0]}, ${LuvNewNew[1]}, ${LuvNewNew[2]}`);
-let RGBNewNew = converter.Luv_to_RGB([50, 0, 0]);
+const RGBNewNew = converter.Luv_to_RGB([50, 0, 0]);
 console.log(`Luv 50, 0, 0=RGB${RGBNewNew[0]}, ${RGBNewNew[1]}, ${RGBNewNew[2]}`);
 
 helloWorld();
