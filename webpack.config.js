c81c0dcbe72aaab59c900637d56f42a2b483a5db
Marcus Pallinger <git@marcus.pallinger.org>
2021-10-22 14:21:10 +1100

    Rebuild with module stuff.

--- webpack.config.js
+++ webpack.config.js
@@ -1,54 +1,57 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
+const webpack = require("webpack"); // eslint-disable-line
+const path = require("path"); // eslint-disable-line
+const CopyPlugin = require("copy-webpack-plugin"); // eslint-disable-line
 
-module.exports = {
-  entry: "./src/index.ts",
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        include: [
-          path.resolve(__dirname, 'src')
-        ],
-        use: 'ts-loader',
-        exclude: /node_modules/,
+const config = {
+    entry: "./src/index.ts",
+    module: {
+        rules: [
+            {
+                test: /\.ts(x)?$/,
+                loader: "ts-loader",
+                exclude: /node_modules/
+            },
+        ]
+    },
+    resolve: {
+        extensions: [
+            ".tsx",
+            ".ts",
+            ".js"
+        ]
+    },
+    output: {
+        path: path.resolve(__dirname, "dist"), // eslint-disable-line
+        filename: "bundle.js",
+        clean: true,
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"), // eslint-disable-line
+            watch: true,
+        },
+        client: {
+            logging: "info",
+            overlay: {
+                errors: true,
+                warnings: true,
+            },
+        },
+        hot: true,
+        port: 4000,
+        bonjour: true,
 
-      },
-    ],
-  },
-  resolve: {
-    extensions: [".tsx", ".ts", ".js"],
-  },
-  output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "bundle.js",
-    clean: true,
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "dist"),
-      watch: true,
     },
-    client: {
-      logging: "info",
-      overlay: {
-        errors: true,
-        warnings: true,
-      },
-    },
-    hot: true,
-    port: 4000,
-    bonjour: true,
-  },
-  plugins: [
-      new CopyPlugin ({
-          patterns: [
-              {
-                  from: "./*.html",
-                  to: "",
-              }
-          ]
-      }),
-  ],
-  devtool: "eval-source-map"
+    plugins: [
+        new CopyPlugin({
+            patterns: [
+                {
+                    from: "./*.html",
+                    to: "",
+                }
+            ]
+        })
+    ]
 };
+
+module.exports = config; // eslint-disable-line
