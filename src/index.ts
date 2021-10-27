d473ec183fd7aae3885a3a5364c6de78a1fe771d
Marcus Pallinger <git@marcus.pallinger.org>
2021-10-27 17:50:29 +1100

    Trial animation

--- src/index.ts
+++ src/index.ts
@@ -12,16 +12,31 @@ console.log(`RGB 119, 119, 119=Luv${LuvNewNew[0]}, ${LuvNewNew[1]}, ${LuvNewNew[
 const RGBNewNew = converter.Luv_to_RGB([50, 0, 0]);
 console.log(`Luv 50, 0, 0=RGB${RGBNewNew[0]}, ${RGBNewNew[1]}, ${RGBNewNew[2]}`);
 
-const generatedGrid = setupCalibration([50, 0, 0], [50, 175, 25], Quadrant.North);
 
 const el = document.querySelector<HTMLCanvasElement>("#calibrationCanvas");
 if (el === null) {
     throw new Error("Could not find the canvas.");
 }
 const setup = new CalibrationImage(el);
+const requestAnimationFrame=window.requestAnimationFrame;
+
+let lastTimer=0;
+
+function drawCalibrate(timer:number):void {
+    if (timer-lastTimer > 50)
+    {
+        console.log(`${timer}`);
+        const generatedGrid = setupCalibration([50, 0, 0], [50, 175, 25], Quadrant.North);
+        setup.initialiseCanvas(generatedGrid);
+        lastTimer=timer;
+    }
+    requestAnimationFrame(drawCalibrate);
+
+}
 
 
+requestAnimationFrame(drawCalibrate);
 
-setup.initialiseCanvas(generatedGrid);
+
 
 helloWorld();
