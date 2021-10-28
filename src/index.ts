89f574b40e7b11c7d0c160692cf38bf83499564b
Marcus Pallinger <git@marcus.pallinger.org>
2021-10-28 20:57:38 +1100

    Change colour

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,7 @@ if (calibrationHost===null) {
     throw new Error("Can't find canvashost");
 }
 
-const tester=new CalibrationTest(calibrationHost,[50, 0, 0], [50, -11.5396, -121.9686]);
+const tester=new CalibrationTest(calibrationHost,[50, 0, 0], [50, -120, 25]);
 const promise=tester.getCalibrationResult();
 promise.then((result)=>{console.log(`${result?"true":"false"}`);});
 console.log("foo");
