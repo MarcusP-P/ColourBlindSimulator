6616f69ca423b06ff3c0f2639251c956e969d26b
Marcus Pallinger <git@marcus.pallinger.org>
2021-11-17 19:29:43 +1100

    Format file.

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import {CalibrationTest} from "./calibration/CalibrationTest";
+import { CalibrationTest } from "./calibration/CalibrationTest";
 
-import {ColorConverter} from "cie-colorconverter";
+import { ColorConverter } from "cie-colorconverter";
 
-const x=0.403;
-const y=0.442;
 const approach1Button = document.querySelector<HTMLButtonElement>("#approach1");
 
 if (approach1Button=== null) {
@@ -35,18 +33,14 @@ const Y = 0.5;
 const X = Y / y * x;
 const Z = Y / y * (1 - x - y);
 
-const Y=0.5;
-const X=Y/y*x;
-const Z=Y/y*(1-x-y);
-    
 const converter = new ColorConverter();
 
 converter.RefWhite = "D65";
-converter.GammaModel="sRGB";
+converter.GammaModel = "sRGB";
 
-const  Luv=converter.XYZ_to_Luv([X, Y, Z]);
+const Luv = converter.XYZ_to_Luv([X, Y, Z]);
 
-console.count(`Luv=[${Luv[0]}, ${Luv[1]}, ${Luv[2]}], u'=${4*X/(X+15*Y+3*Z)} v'=${9*Y/(X+15*Y+3*Z)}`);
+console.count(`Luv=[${Luv[0]}, ${Luv[1]}, ${Luv[2]}], u'=${4 * X / (X + 15 * Y + 3 * Z)} v'=${9 * Y / (X + 15 * Y + 3 * Z)}`);
 
 
 
